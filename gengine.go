a16a097498c077011c67e6d3ec3347c810612b54
o0PixelDev0o <80274470+o0PixelDev0o@users.noreply.github.com>
2021-08-22 14:13:31 -0400

    Add files via upload

--- gengine.go
+++ gengine.go
@@ -1,7 +1,32 @@
 package gengine
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/pixelgl"
+	"golang.org/x/image/colornames"
+)
 
 func Greet(){
 	fmt.Println("GENGINE")
 }
+
+func NewWindow(Title string){
+	cfg := pixelgl.WindowConfig{
+		//Title:  "Pixel Rocks!",
+		Bounds: pixel.R(0, 0, 1024, 768),
+		VSync:  true,
+	}
+
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+	
+	win.Clear(colornames.Skyblue)
+
+	for !win.Closed() {
+		win.Update()
+	}
+}
+
